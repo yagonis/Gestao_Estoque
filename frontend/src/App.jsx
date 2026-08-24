5f7877fd61ad7bf7b1792023d39544003fb7463f
Yago Brito <yago.brito@ufvjm.edu.br>
2026-08-24 15:55:38 -0300

    Divisão do back-end com o front-end, criação das pastas principais do front-end e importação do tailwindcss e shadcn para módulos prontos de css.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {Button} from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Gestão de Estoque
+      </h1>
+      <Button>Adicionar Produto</Button>
+    </div>
+  )
+}
+
+export default App
